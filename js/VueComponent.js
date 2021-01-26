5cb99bc4bb5b7fd94d49c5ccb21cab2c2878edc8
Gustavo <f170015@gmail.com>
2021-01-25 23:18:04 -0600

    Pagina Terminada, antes de subir a un host

--- js/VueComponent.js
+++ js/VueComponent.js
@@ -1,6 +1,6 @@
-Vue.component('about-card',{
-    props: ['img', 'tittle', 'info', 'repo'],
-    template: `
+Vue.component('about-card', {
+	props: ['img', 'tittle', 'info', 'repo'],
+	template: `
     <div class="col col-lg-3 mt-3 animacion">
         <div class="card h-100 border-light mg-fluid">
             
@@ -18,92 +18,92 @@ Vue.component('about-card',{
             </div>
         </div>
     </div>
-    `
-})
+    `,
+});
 
 new Vue({
-    el: '#Component-conocimientos',
-    data: {
-        conocimientos: [
-            {
-                nombre: 'java',
-                img: 'public/img/java.png',
-                info: 'Conocimientos en java',
-            },
-            {
-                nombre: 'JavaScript',
-                img: 'public/img/java-script.png',
-                info:'Programacion con nuevos estandares JavaScript ES+',
-                repo: 'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=javascript'
-            },
-            {
-                nombre: 'Python',
-                img: 'public/img/python.png',
-                info:'Conocimientos en python, conexion a base de datos y framework flask',
-            },
-            {
-                nombre: 'Base de Datos',
-                img: 'public/img/dato.png',
-                info:'Manejo de Base de datos por comando CIUD'
-            },
-            {
-                nombre: 'PHP',
-                img: 'public/img/php.png',
-                info:'Implementacion de PHP como BackEnd en paginas web',
-                repo: 'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=php'
-            },
-            {
-                nombre: 'Creacion de Paginas Web',
-                img: 'public/img/navegador.png',
-                info:'Creacion de Paginas Web implementando HTML5, CSS3, Bootstrap 4',
-                repo: 'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=html'
-            },
-            {
-                nombre: 'ReactJS',
-                img: 'public/img/reactjs.png',
-                info: 'Conocimientos Basicos de ReactJs con HOOKS y MERS',
-                repo: 'https://github.com/Gustavo-system/Buscador-Gifs-React-app'
-            },
-            {
-                nombre: 'Controlador de Versiones',
-                img: 'public/img/github.png',
-                info:'Manejo de GIT en GitHub',
-                repo:'https://github.com/Gustavo-system'
-            }
-
-
-        ]
-    }
-})
+	el: '#Component-conocimientos',
+	data: {
+		conocimientos: [
+			{
+				nombre: 'java',
+				img: 'public/img/java.png',
+				info: 'Conocimientos en java',
+			},
+			{
+				nombre: 'JavaScript',
+				img: 'public/img/java-script.png',
+				info: 'Programacion con nuevos estandares JavaScript ES+',
+				repo:
+					'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=javascript',
+			},
+			{
+				nombre: 'Python',
+				img: 'public/img/python.png',
+				info: 'Conocimientos en python, conexion a base de datos',
+			},
+			{
+				nombre: 'Base de Datos',
+				img: 'public/img/dato.png',
+				info: 'Manejo de Base de datos por comando CRUD',
+			},
+			{
+				nombre: 'PHP',
+				img: 'public/img/php.png',
+				info: 'Implementacion de PHP como BackEnd en paginas web',
+				repo: 'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=php',
+			},
+			{
+				nombre: 'Creacion de Paginas Web',
+				img: 'public/img/navegador.png',
+				info: 'Creacion de Paginas Web implementando HTML5, CSS3, Bootstrap 4',
+				repo: 'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=html',
+			},
+			{
+				nombre: 'ReactJS',
+				img: 'public/img/reactjs.png',
+				info: 'Conocimientos Basicos de ReactJs con HOOKS y MERS',
+				repo: 'https://github.com/Gustavo-system/Buscador-Gifs-React-app',
+			},
+			{
+				nombre: 'Controlador de Versiones',
+				img: 'public/img/github.png',
+				info: 'Manejo de GIT en GitHub',
+				repo: 'https://github.com/Gustavo-system',
+			},
+		],
+	},
+});
 
 new Vue({
-    el: '#Componente-aprendiendo',
-    data: {
-        aprendiendo: [
-            {
-                nombre: 'Desarrollo Movil',
-                img: 'public/img/androide.png',
-                info:'Estoy aprendiendo a desarrollar aplicaciones nativas para Android, utilizando Java o Kotlin.',
-                repo: 'https://github.com/Gustavo-system/my-pizza-app'
-            },
-            {
-                nombre: 'React Native',
-                img: 'public/img/react-native.png',
-                info:'Estoy aprendiendo a desarrollar aplicaciones para IOS y Android con React Native ya que el codigo se puede reutilizar y ahorrar tiempos de elavoracion',
-            },
-            {
-                nombre: 'Angular',
-                img: 'public/img/angular.png',
-                info:'Creacion de Aplicaciones Web implementando Angular CLI',
-                repo:'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=typescript'
-            },
-            {
-                nombre: 'VueJs',
-                img: 'public/img/vue.png',
-                info:'Creacion de Aplicaciones Web implementando Vue',
-            }
-
-
-        ]
-    }
-})
+	el: '#Componente-aprendiendo',
+	data: {
+		aprendiendo: [
+			{
+				nombre: 'Desarrollo Movil',
+				img: 'public/img/androide.png',
+				info:
+					'Estoy aprendiendo a desarrollar aplicaciones nativas para Android, utilizando Java o Kotlin.',
+				repo: 'https://github.com/Gustavo-system/my-pizza-app',
+			},
+			{
+				nombre: 'React Native',
+				img: 'public/img/react-native.png',
+				info:
+					'Estoy aprendiendo a desarrollar aplicaciones para IOS y Android con React Native ya que el codigo se puede reutilizar y ahorrar tiempos de elavoracion',
+			},
+			{
+				nombre: 'Angular',
+				img: 'public/img/angular.png',
+				info: 'Creacion de Aplicaciones Web implementando Angular CLI',
+				repo:
+					'https://github.com/Gustavo-system?tab=repositories&q=&type=&language=typescript',
+			},
+			{
+				nombre: 'VueJs',
+				img: 'public/img/vue.png',
+				info: 'Creacion de Aplicaciones Web implementando Vue',
+			},
+		],
+	},
+});
--- js/formulario.js
+++ js/formulario.js
@@ -1,98 +1,94 @@
-class UI{
+class UI {
+	static alerta(etiqueta, classN) {
+		const el = document.querySelector(etiqueta);
+		el.className = `form-control text-uppercase ${classN}`;
+	}
 
-    static alerta(etiqueta, classN){
-        const el = document.querySelector(etiqueta);
-        el.className = `form-control text-uppercase ${classN}`;
-    }
+	static limpiarCampos() {
+		document.querySelector('#id_nombre').value = '';
+		document.querySelector('#id_empresa').value = '';
+		document.querySelector('#id_phoneNumber').value = '';
+		document.querySelector('#id_email').value = '';
+		document.querySelector('#id_asunto').value = '';
+	}
 
-    static limpiarCampos(){
-        document.querySelector('#id_nombre').value = '';
-        document.querySelector('#id_empresa').value = '';
-        document.querySelector('#id_phoneNumber').value = '';
-        document.querySelector('#id_email').value = '';
-        document.querySelector('#id_asunto').value = '';
-    }
+	static error(id) {
+		const el = document.getElementById(id);
+		el.style.opacity = 1;
+	}
 
-    static error(id){
-        const el = document.getElementById(id);
-        el.style.opacity = 1;
-    }
-
-    static good(id){
-        const el = document.getElementById(id);
-        el.style.opacity = 0;
-    }
+	static good(id) {
+		const el = document.getElementById(id);
+		el.style.opacity = 0;
+	}
 }
 
+function validar() {
+	let nombre = document.getElementById('id_nombre').value;
+	let empresa = document.getElementById('id_empresa').value;
+	let numero = document.getElementById('id_phoneNumber').value;
+	let email = document.getElementById('id_email').value;
 
-function validar(){
-    let nombre = document.getElementById('id_nombre').value;
-    let empresa = document.getElementById('id_empresa').value;
-    let numero = document.getElementById('id_phoneNumber').value;
-    let email = document.getElementById('id_email').value;
+	const r_nombre = validarNombre(nombre);
+	const r_empresa = validarEmpresa(empresa);
+	const r_numero = validarNumero(numero);
+	const r_email = validarEmail(email);
 
-    const r_nombre = validarNombre(nombre);
-    const r_empresa = validarEmpresa(empresa);
-    const r_numero = validarNumero(numero);
-    const r_email = validarEmail(email);
-
-    if(r_nombre == false || r_empresa == false || r_numero == false || r_email == false){
-        //alert('Comprueba los datos solicitados');
-        return false;
-    }
-    else{
-        alert('datos enviados');
-    }
-
+	if (r_nombre == false || r_empresa == false || r_numero == false || r_email == false) {
+		//alert('Comprueba los datos solicitados');
+		return false;
+	} else {
+		alert('datos enviados');
+	}
 }
 
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+};
+
+const { nombre, correo } = expresiones;
 
 const validarNombre = (el) => {
-    if(el === "" || el.length < 10){
-        UI.alerta('#id_nombre', 'is-invalid');
-        UI.error('error_nombre');
-        return false;
-    }
-    else{
-        UI.alerta('#id_nombre', 'is-valid');
-        UI.good('error_nombre');
-    }
-}
+	if (el === '' || el.length < 10 || !nombre.test(el)) {
+		UI.alerta('#id_nombre', 'is-invalid');
+		UI.error('error_nombre');
+		return false;
+	} else {
+		UI.alerta('#id_nombre', 'is-valid');
+		UI.good('error_nombre');
+	}
+};
 
-const validarEmpresa = (el) =>{
-    if(el === "" || el.length < 10){
-        UI.alerta('#id_empresa', 'is-invalid');
-        UI.error('error_empresa');
-        return false;
-    }
-    else{
-        UI.alerta('#id_empresa', 'is-valid');
-        UI.good('error_empresa');
-    }
-}
+const validarEmpresa = (el) => {
+	if (el === '' || el.length < 10) {
+		UI.alerta('#id_empresa', 'is-invalid');
+		UI.error('error_empresa');
+		return false;
+	} else {
+		UI.alerta('#id_empresa', 'is-valid');
+		UI.good('error_empresa');
+	}
+};
 
-const validarNumero = (numero) =>{
-    if(numero === "" || numero.length != 10 || isNaN(numero)){
-        UI.alerta('#id_phoneNumber', 'is-invalid');
-        UI.error('error_numero');
-        return false;
-    }
-    else{
-        UI.alerta('#id_phoneNumber', 'is-valid');
-        UI.good('error_numero');
-    }
-}
+const validarNumero = (numero) => {
+	if (numero === '' || numero.length != 10 || isNaN(numero)) {
+		UI.alerta('#id_phoneNumber', 'is-invalid');
+		UI.error('error_numero');
+		return false;
+	} else {
+		UI.alerta('#id_phoneNumber', 'is-valid');
+		UI.good('error_numero');
+	}
+};
 
-const validarEmail = (email) =>{
-    if(email === "" || email.length < 10){
-        UI.alerta('#id_email', 'is-invalid');
-        UI.error('error_email');
-        return false;
-    }
-    else{
-        UI.alerta('#id_email', 'is-valid');
-        UI.good('error_email');
-    }
-}
-
-
+const validarEmail = (email) => {
+	if (email === '' || email.length < 10 || !correo.test(email)) {
+		UI.alerta('#id_email', 'is-invalid');
+		UI.error('error_email');
+		return false;
+	} else {
+		UI.alerta('#id_email', 'is-valid');
+		UI.good('error_email');
+	}
+};
